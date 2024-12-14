ccd392750819cb8702ca4e995458348e823c6603
MostOfLuck <peterbaikov12@gmail.com>
2024-12-14 12:29:05 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ function App() {
 
   return (
     <>
+<div className='absolute top-0 left-0 w-full h-20 bg-blue-100'></div>
+<div className='flex absolute top-0 left-0 m-4'>
+  <img className='w-12 bottom-2 relative' src="/public/worker.png" alt="" />
+  <h1 className='text-3xl'>newjobs</h1>
+</div>
       <Login />
     </>
   )
