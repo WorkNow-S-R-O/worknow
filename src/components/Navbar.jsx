b03af9080e4c7f24421bbf7f0a698628e4107e8b
MostOfLuck <peterbaikov12@gmail.com>
2024-12-15 15:58:09 +0200

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <>
     <div className='absolute top-0 left-0 w-full h-16 bg-blue-100'></div><div className='flex absolute top-0 left-0 m-3'>
           <img className='w-12 bottom-2 relative' src="/public/worker.png" alt="" />
-          <h1 className='text-3xl'>newjobs</h1>
+          <h1 className='text-3xl'>worknow</h1>
       </div>
       
       <button type="button" className="btn btn-primary flex absolute top-0 right-0 m-3 ">Login</button>
