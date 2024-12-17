b563b8389f94bc67136b9cd2b74ac6cbd8337128
MostOfLuck <peterbaikov12@gmail.com>
2024-12-17 19:32:52 +0200

    add i18next

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,121 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useTranslation } from "react-i18next";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const Navbar = () => {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <>
       {/* Desktop Version */}
-      <div className='d-none d-lg-block'>
-        <div className='absolute top-0 left-0 w-full h-16 bg-[#e3f2fd]'></div>
-        <div className='flex absolute top-0 left-0 m-3'>
-        <Link to="/" className="d-flex align-items-center no-underline text-black">
-      <img className='w-12 bottom-2 relative ml-0' src="/assets/images/worker.png" alt="Logo" />
-      <h1 className='text-3xl ml-0'>worknow</h1>
-       </Link>
-          <ul className='flex justify-center items-center ml-20 gap-2 mb-2 text-gray-500'>
-            <li className='mr-3'>
-              <Link to="/vacancies" className='text-lg font-normal text-gray-600 hover:text-gray-900 no-underline'>Vacancies</Link>
+      <div className="d-none d-lg-block">
+        <div className="absolute top-0 left-0 w-full h-16 bg-[#e3f2fd]"></div>
+        <div className="flex absolute top-0 left-0 m-3">
+          <Link
+            to="/"
+            className="d-flex align-items-center no-underline text-black"
+          >
+            <img
+              className="w-12 bottom-2 relative ml-0"
+              src="/assets/images/worker.png"
+              alt="Logo"
+            />
+            <h1 className="text-3xl ml-0">worknow</h1>
+          </Link>
+          <ul className="flex justify-center items-center ml-20 gap-2 mb-2 text-gray-500">
+            <li className="mr-3">
+              <Link
+                to="/"
+                className="text-lg font-normal text-gray-600 hover:text-gray-900 no-underline"
+              >
+                {t("community")}
+              </Link>
             </li>
-            <li className='mr-3'>
-              <Link to="/about" className='text-lg font-normal text-gray-600 hover:text-gray-900 no-underline'>Job map</Link>
+            <li className="mr-3">
+              <Link
+                to="/jobs"
+                className="text-lg font-normal text-gray-600 hover:text-gray-900 no-underline"
+              >
+                {t("jobs")}
+              </Link>
             </li>
             <li>
-              <Link to="/contact" className='text-lg font-normal text-gray-600 hover:text-gray-900 no-underline'>Facebook groups</Link>
+              <Link
+                to="/facebook"
+                className="text-lg font-normal text-gray-600 hover:text-gray-900 no-underline"
+              >
+                {t("facebookgroups")}
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/salaries"
+                className="text-lg m-2.5 font-normal text-gray-600 hover:text-gray-900 no-underline"
+              >
+                {t("salaries")}
+              </Link>
             </li>
           </ul>
         </div>
-        <button type="button" className="btn btn-primary flex absolute top-0 right-0 m-3">Login</button>
-        <button type="button" className="btn btn-warning flex absolute top-0 right-20 m-3">Premium</button>
+        <div className="d-flex align-items-center absolute top-0 right-0 m-3">
+          <div className="dropdown me-2">
+            <button
+              className="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="languageDropdown"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Language
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="languageDropdown">
+              <li>
+                <a
+                  onClick={() => changeLanguage("en")}
+                  className="dropdown-item"
+                  href="#"
+                >
+                  English
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={() => changeLanguage("ru")}
+                  className="dropdown-item"
+                  href="#"
+                >
+                  Russian
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button type="button" className="btn btn-primary me-2">
+            {t("login")}
+          </button>
+          <button type="button" className="btn btn-warning">
+            {t("premium")}
+          </button>
+        </div>
       </div>
 
       {/* Mobile Version */}
-      <nav className="navbar navbar-expand-lg navbar-light bg-light d-lg-none fixed-top">
+      <nav className="navbar navbar-expand-lg navbar-light bg-[#e3f2fd] d-lg-none fixed-top">
         <div className="container-fluid">
           <Link className="navbar-brand d-flex align-items-center" to="/">
-            <img className='w-12 me-2' src="/assets/images/worker.png" alt="Logo" />
-            <h1 className='text-3xl m-0'>worknow</h1>
+            <img
+              className="w-12 me-2"
+              src="/assets/images/worker.png"
+              alt="Logo"
+            />
+            <h1 className="text-3xl m-0">worknow</h1>
           </Link>
 
           <button
@@ -50,22 +129,86 @@ const Navbar = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className={`navbar-collapse ${isExpanded ? "show" : "collapse"}`} id="navbarNav">
+          <div
+            className={`navbar-collapse ${isExpanded ? "show" : "collapse"}`}
+            id="navbarNav"
+          >
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className='nav-link text-lg font-normal text-gray-600 hover:text-gray-900 no-underline' to="/vacancies">Vacancies</Link>
+                <Link
+                  className="nav-link text-lg font-normal text-gray-600 hover:text-gray-900 no-underline"
+                  to="/"
+                >
+                  {t("community")}
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className='nav-link text-lg font-normal text-gray-600 hover:text-gray-900 no-underline' to="/about">Job map</Link>
+                <Link
+                  className="nav-link text-lg font-normal text-gray-600 hover:text-gray-900 no-underline"
+                  to="/jobs"
+                >
+                  {t("jobs")}
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className='nav-link text-lg font-normal text-gray-600 hover:text-gray-900 no-underline' to="/contact">Facebook groups</Link>
+                <Link
+                  className="nav-link text-lg font-normal text-gray-600 hover:text-gray-900 no-underline"
+                  to="/facebook"
+                >
+                  {t("facebookgroups")}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link text-lg font-normal text-gray-600 hover:text-gray-900 no-underline"
+                  to="/salaries"
+                >
+                  {t("salaries")}
+                </Link>
               </li>
             </ul>
 
             <div className="d-flex flex-column gap-2 mt-3">
-              <button type="button" className="btn btn-primary">Login</button>
-              <button type="button" className="btn btn-warning">Premium</button>
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="mobileLanguageDropdown"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Language
+                </button>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="mobileLanguageDropdown"
+                >
+                  <li>
+                    <a
+                      onClick={() => changeLanguage("en")}
+                      className="dropdown-item"
+                      href="#"
+                    >
+                      English
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={() => changeLanguage("ru")}
+                      className="dropdown-item"
+                      href="#"
+                    >
+                      Russian
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <button type="button" className="btn btn-primary">
+                {t("login")}
+              </button>
+              <button type="button" className="btn btn-warning">
+                {t("premium")}
+              </button>
             </div>
           </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import NotFoundPage from './pages/NotFoundPage.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './pages/App.jsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./pages/App.jsx";
 
+import './18n.js';
 
 const router = createBrowserRouter([
   {
-  path: '/',
-  element: <App />,
-  errorElement: <NotFoundPage />
-},
-
+    path: "/",
+    element: <App />,
+    errorElement: <NotFoundPage />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
