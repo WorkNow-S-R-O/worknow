8b6748d70a9db116fcef0aa1221326bcdaf0ef51
MostOfLuck <peterbaikov12@gmail.com>
2024-12-18 13:54:43 +0200

    update

--- hooks/useLanguageSwitcher.jsx
+++ hooks/useLanguageSwitcher.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate, useLocation } from "react-router-dom";
 
@@ -6,12 +7,30 @@ export const useLanguageSwitcher = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const changeLanguage = (language) => {
-    i18n.changeLanguage(language);
-
+  // Установка языка при загрузке на основе URL
+  useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
-    searchParams.set("lng", language);
-    navigate(`${location.pathname}?${searchParams.toString()}`);
+    const urlLanguage = searchParams.get("lng");
+
+    if (urlLanguage && urlLanguage !== i18n.language) {
+      i18n.changeLanguage(urlLanguage);
+    } else if (!urlLanguage) {
+      // Если языка нет в URL, добавляем текущий язык
+      searchParams.set("lng", i18n.language);
+      navigate(`${location.pathname}?${searchParams.toString()}`, {
+        replace: true,
+      });
+    }
+  }, [location.search, i18n, navigate, location.pathname]);
+
+  const changeLanguage = (language) => {
+    if (i18n.language !== language) {
+      i18n.changeLanguage(language);
+
+      const searchParams = new URLSearchParams(location.search);
+      searchParams.set("lng", language);
+      navigate(`${location.pathname}?${searchParams.toString()}`);
+    }
   };
 
   return { changeLanguage };
