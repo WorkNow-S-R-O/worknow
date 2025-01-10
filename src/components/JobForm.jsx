596304fb5f136d30939eec7e01c484f09c79202b
MostOfLuck <peterbaikov12@gmail.com>
2025-01-10 21:34:28 +0200

    update

--- src/components/JobForm.jsx
+++ src/components/JobForm.jsx
@@ -51,6 +51,20 @@ const JobForm = () => {
               required
             />
           </div>
+
+          <div className="mb-4">
+            <label htmlFor="number" className="block text-gray-700 mb-2">
+              {t("phone_number")}
+            </label>
+            <input
+              id="number"
+              type="text"
+              name="number"
+              className="bg-white w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder={t("write_phone_number")}
+              required
+            />
+          </div>
           <div className="mb-4">
             <label htmlFor="description" className="block text-gray-700 mb-2">
               {t("description")}
