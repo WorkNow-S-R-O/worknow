c2f67db6fae803692d10bd528ad9dbf70f32b0fa
MostOfLuck <peterbaikov12@gmail.com>
2025-02-15 17:46:41 +0200

    update src/pages/NotFoundPage.jsx

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -8,14 +8,13 @@ export default function NotFoundPage() {
     <div className="h-screen flex justify-center items-center flex-col">
       <img
         className="w-full md:w-1/3"
-        src="./images/404.jpg"
-        alt="error"
+        src="/images/404.jpg"
+        alt="not-found"
       />
-      <div className="btn btn-primary mt-4 md:mt-6 text-sm md:text-lg">
-        <Link to="/" className="text-white no-underline">
-          <h1 className="md:text-5xl text-sm ">{t("backtohome")}</h1>
-        </Link>
-      </div>
+      <p className="md:text-3xl text-sm mt-4">{t("page_not_found")}</p>
+      <Link to="/" className="btn btn-primary mt-4 md:mt-6 text-white no-underline">
+        <h1 className="md:text-2xl text-sm">{t("backtohome")}</h1>
+      </Link>
     </div>
   );
 }
