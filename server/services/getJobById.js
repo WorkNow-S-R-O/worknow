285092d26f535328fe364c264fd60c0c28f45613
MostOfLuck <peterbaikov12@gmail.com>
2025-03-03 16:43:22 +0200

    update

--- server/services/getJobById.js
+++ server/services/getJobById.js
@@ -4,17 +4,22 @@ const prisma = new PrismaClient();
 
 export const getJobByIdService = async (id) => {
   try {
+    if (!id) {
+      throw new Error("ID вакансии не передан");
+    }
+
     const job = await prisma.job.findUnique({
-      where: { id: parseInt(id) },
+      where: { id: Number(id) }, // Приводим id к числу
       include: { city: true },
     });
 
     if (!job) {
-      return { error: 'Объявление не найдено' };
+      return { error: "Объявление не найдено" };
     }
 
     return { job };
   } catch (error) {
-    return { error: 'Ошибка получения объявления', details: error.message };
+    console.error("Ошибка в getJobByIdService:", error);
+    return { error: "Ошибка получения объявления", details: error.message };
   }
 };
