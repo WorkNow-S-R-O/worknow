442eaf3997aef15d1a758bad19608e76b0443e34
MostOfLuck <peterbaikov12@gmail.com>
2025-03-03 18:23:21 +0200

    update

--- server/utils/stripe.js
+++ server/utils/stripe.js
@@ -2,6 +2,7 @@ import Stripe from 'stripe';
 import dotenv from 'dotenv';
 
 dotenv.config();
+console.log("📌 Stripe API Key:", process.env.STRIPE_SECRET_KEY ? "✅ Найден" : "❌ Отсутствует");
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
