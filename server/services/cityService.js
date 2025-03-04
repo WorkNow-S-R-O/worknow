bd5838667eb6895605e9b7631d1770c15a1a7821
MostOfLuck <peterbaikov12@gmail.com>
2025-03-04 19:53:15 +0200

    update

--- server/services/cityService.js
+++ server/services/cityService.js
@@ -1,4 +1,6 @@
-import prisma from '../prismaClient.js';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const getCitiesService = async () => {
   try {
